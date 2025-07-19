49995479f61291da7393cdc9a47950519c49c7c7
ijeh03 <155229744+ijeh03@users.noreply.github.com>
2025-07-19 22:09:39 +0100

    Add files via upload

---
+++ t and t catering/index.js
@@ -0,0 +1,52 @@
+let students = 90; 80; 65; 55; 40; 39;
+
+let a = 90;
+let b = 80;
+let c = 65;
+let d = 55;
+let e = 40;
+let f = 39;
+ 
+
+if (students == 90 ) {
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+if (students == 80 ) {
+    console.log("true");
+}else{
+    console.log("false");
+
+}if (students == 65 ) {
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+if (students == 55 ) {
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+if (students == 40 ) {
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+if (students == 39 ) {
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+
+
+
+
+
+
+
